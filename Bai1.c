a7c5358504cf281672a07381c4fd7e433337af28
nguyenvan1604 <xoxo2team@gmail.com>
2018-03-17 22:35:13 +0700

    change

---
+++ Bai1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float f(int n)
+{
+    if(n<2)
+        return 1;
+    if(n>=2 && n<10)
+        return (f(n-1))/(n-10) + f(n-2);
+    if(n==10)
+        return 0;
+    return f(n-1) + f(n-2) + 4/5;
+}
+int main()
+{
+    int n;
+    printf("Nhap n: ");
+    scanf("%d",&n);
+    printf("%.2f",f(n));
+
+    printf("\n");
+    return 0;
+}
